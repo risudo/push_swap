e222b9047001d7c1f73de0610fcb89cea32ede80
rsudo <badbad7351@gmail.com>
2022-02-06 22:14:07 +0900

    update error_tes.sh

--- error_test.sh
+++ error_test.sh
@@ -3,16 +3,17 @@
 GREEN="\033[32m"
 RESET="\033[39m"
 
-execute() {
-	echo -e "$GREEN" "[[" ${1} ${2} "]]" "$RESET"
-	./push_swap $1 $2
-	echo
+execute () {
+	echo -e ${GREEN}${1}${RESET}
+	eval $1
+	echo ""
 }
 
-execute aa
-execute 1a
-execute 1 1
-execute 2147483648 1
-execute -2147483649 1
-execute ""
-
+execute './push_swap aa'
+execute './push_swap 1a'
+execute './push_swap 2147483648 1'
+execute './push_swap -2147483649 1'
+execute './push_swap 4294967300 1'
+execute './push_swap 1 1'
+execute './push_swap +0 -0'
+execute './push_swap'
